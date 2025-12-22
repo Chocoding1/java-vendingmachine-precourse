3fe32bd73ece1b1ca174f82598ec247ed006aadd
Chocoding1 <galmeagi2@naver.com>
2025-12-22 22:52:53 +0900

    Refactor: 로직 수정

--- src/main/java/vendingmachine/controller/VendingController.java
+++ src/main/java/vendingmachine/controller/VendingController.java
@@ -3,13 +3,12 @@ package vendingmachine.controller;
 import static vendingmachine.model.ExceptionHandler.*;
 
 import java.util.EnumMap;
-import java.util.List;
 import vendingmachine.model.Coin;
 import vendingmachine.model.ChangeProvider;
-import vendingmachine.model.ExceptionHandler;
 import vendingmachine.model.PayAmount;
 import vendingmachine.model.Product;
 import vendingmachine.model.ProductParser;
+import vendingmachine.model.Products;
 import vendingmachine.model.RandomCoinGenerator;
 import vendingmachine.model.VendingMoney;
 import vendingmachine.model.VendingProcessor;
@@ -37,16 +36,14 @@ public class VendingController {
         VendingMoney vendingMoney = repeatUntilSuccess(this::getVendingMoney);
         EnumMap<Coin, Integer> availableCoins = randomCoinGenerator.generate(vendingMoney);
         outputView.printAvailableCoins(availableCoins);
-        String input = inputView.getProductsInfo();
-        List<Product> products = productParser.parse(input); // 예외 처리 필요
-        int minPrice = getMinPrice(products);
+        Products products = repeatUntilSuccess(this::getProducts);
+        int minPrice = products.minPrice();
         PayAmount payAmount = repeatUntilSuccess(this::getPayAmount);
 
         while (true) {
             outputView.printRemainAmount(payAmount);
 
-            String productName = inputView.getProductName();
-            Product findProduct = repeatUntilSuccess(() -> findProduct(products, productName, payAmount));
+            Product findProduct = repeatUntilSuccess(() -> findProduct(products, payAmount));
             vendingProcessor.sell(findProduct, payAmount);
 
             if (payAmount.isLess(minPrice)) {
@@ -65,12 +62,9 @@ public class VendingController {
         return new VendingMoney(initialInput);
     }
 
-    private int getMinPrice(List<Product> products) {
-        int minPrice = Integer.MAX_VALUE;
-        for (Product product : products) {
-            minPrice = Math.min(minPrice, product.getPrice());
-        }
-        return minPrice;
+    private Products getProducts() {
+        String input = inputView.getProductsInfo();
+        return productParser.parse(input);
     }
 
     private PayAmount getPayAmount() {
@@ -78,17 +72,8 @@ public class VendingController {
         return new PayAmount(input);
     }
 
-    // 최적화 필수 메서드
-    private Product findProduct(List<Product> products, String productName, PayAmount payAmount) {
-        for (Product product : products) {
-            if (product.isSameName(productName)) {
-                product.checkPrice(payAmount);
-                if (product.isSoldOut()) {
-                    throw new IllegalArgumentException("[ERROR] 상품이 매진되었습니다.");
-                }
-                return product;
-            }
-        }
-        throw new IllegalArgumentException("[ERROR] 존재하지 않는 상품입니다.");
+    private Product findProduct(Products products, PayAmount payAmount) {
+        String productName = inputView.getProductName();
+        return products.find(productName, payAmount);
     }
 }
