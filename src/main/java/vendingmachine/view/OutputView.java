eb02bbebf792877e199bfc781ff94435c7431047
Chocoding1 <galmeagi2@naver.com>
2025-12-22 15:52:23 +0900

    Refactor: 출력 형식 수정

--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -7,7 +7,7 @@ import vendingmachine.model.PayAmount;
 public class OutputView {
 
     private static final String AVAILABLE_COINS_PRINT_FORMAT = "자판기가 보유한 동전";
-    private static final String REMAIN_AMOUNT_PRINT_FORMAT = "투입 금액: ";
+    private static final String REMAIN_AMOUNT_PRINT_FORMAT = "투입 금액: %d원\n";
     private static final String CHANGE_PRINT_FORMAT = "잔돈";
 
     public void printAvailableCoins(EnumMap<Coin, Integer> availableCoins) {
@@ -18,7 +18,7 @@ public class OutputView {
     }
 
     public void printRemainAmount(PayAmount payAmount) {
-        System.out.println(REMAIN_AMOUNT_PRINT_FORMAT + payAmount.getAmount());
+        System.out.printf(REMAIN_AMOUNT_PRINT_FORMAT, payAmount.getAmount());
     }
 
     public void printChange(EnumMap<Coin, Integer> change) {
